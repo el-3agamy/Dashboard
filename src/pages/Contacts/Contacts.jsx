077c15ccbfc6aae0671bde8d12294bec0587d8cb
el-3agamy <Selagamy25@gmail.com>
2025-04-30 15:47:47 +0300

    built profile & team pages

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import rows from '../../Data/TeamData';
+import {columns} from '../../Data/TeamData';
+import { Toolbar, ToolbarButton } from '@mui/x-data-grid';
+
+
 
 const Contacts = () => {
+ 
   return (
-    <div>Contacts</div>
+    <Box sx={{ height: 500, width: '100%' }}>
+    <DataGrid
+      rows={rows}
+      columns={columns}
+
+        slots={{ toolbar: GridToolbar , }}
+        
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 7,
+          },
+        },
+      }}
+      pageSizeOptions={[5]}
+      checkboxSelection
+      disableRowSelectionOnClick
+    />
+  </Box>
   )
 }
 
--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import rows from '../../Data/TeamData';
+import {columns} from '../../Data/TeamData';
+
+
+// console.log(columns.length);
+
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <Box sx={{ height: 500, width: '100%' }}>
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 7,
+          },
+        },
+      }}
+      pageSizeOptions={[5]}
+      checkboxSelection
+      disableRowSelectionOnClick
+    />
+  </Box>
   )
 }
 
